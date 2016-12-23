3a4dc35f1200d6894cf0e130dec0ab97bff89794
cocafe <cocafehj@gmail.com>
2016-12-23 15:35:52 +0800

    add basic resource files

--- ffmpeg_sdl_audio_player/stdafx.h
+++ ffmpeg_sdl_audio_player/stdafx.h
@@ -2,4 +2,11 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <memory.h>
+#include <tchar.h>
+#include <process.h>
+
 #include <Windows.h>
