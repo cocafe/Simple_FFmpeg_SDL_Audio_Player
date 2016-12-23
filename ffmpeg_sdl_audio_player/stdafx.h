9c13ddd7c6b83a6bd566455e72c9c837c207c246
cocafe <cocafehj@gmail.com>
2016-12-23 15:43:42 +0800

    debug: add debug headers

--- ffmpeg_sdl_audio_player/stdafx.h
+++ ffmpeg_sdl_audio_player/stdafx.h
@@ -10,3 +10,20 @@
 #include <process.h>
 
 #include <Windows.h>
+
+#include "resource.h"
+#include "debug.h"
+
+#define MessageBoxInfo(Msg)		do { MessageBox(						\
+										NULL,							\
+										Msg,							\
+										L"Simple ffmpeg player",		\
+										MB_ICONINFORMATION | MB_OK); } while (0)
+
+#define MessageBoxErr(Msg)		do { MessageBox(						\
+										NULL,							\
+										Msg,							\
+										L"Simple ffmpeg player",		\
+										MB_ICONERROR | MB_OK); } while (0)
+
+#define ARRAY_SIZE(a)			(SSIZE_T)(sizeof(a) / sizeof(a[0]))
