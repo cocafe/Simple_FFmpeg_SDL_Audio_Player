08558a946fcfcd4b5fce4f2fd3c67b90e7b6b137
cocafe <cocafehj@gmail.com>
2016-12-25 23:16:29 +0800

    playback_buf: store per frame instead of continuous buffer
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- ffmpeg_sdl_audio_player/playback_buf.c
+++ ffmpeg_sdl_audio_player/playback_buf.c
@@ -21,15 +21,19 @@ void playback_buf_free(PlaybackBuf **buf)
 	*buf = NULL;
 }
 
-int playback_buf_init(PlaybackBuf *buf, int32_t buf_size)
+int playback_buf_init(PlaybackBuf *buf, int32_t per_size, int32_t multiple)
 {
 	if (!buf)
 		return -EINVAL;
 
 	buf->buf_pos = 0;
 	buf->buf_len = 0;
-	buf->buf_size = buf_size;
+	buf->buf_size = per_size * multiple;
 	buf->buf_data = (uint8_t *)calloc(1, buf->buf_size);
+
+	buf->buf_per_size = per_size;
+	buf->buf_per_count = multiple;
+	buf->buf_per_samples = (int32_t *)calloc(multiple, sizeof(int32_t));
 
 	return 0;
 }
@@ -40,12 +44,28 @@ void playback_buf_deinit(PlaybackBuf *buf)
 		return;
 
 	free(buf->buf_data);
+	free(buf->buf_per_samples);
 }
 
-int playback_buf_write(PlaybackBuf *buf, uint8_t *data, int32_t size)
+int playback_buf_write(PlaybackBuf *buf, uint8_t *data, int32_t size, int32_t nb_samples)
 {
+	int idx;
+
+	if (!buf)
+		return -EINVAL;
+
+	if (size != buf->buf_per_size)
+		return -EINVAL;
+
+	/* store nb_samples */
+	idx = buf->buf_pos / buf->buf_per_size;
+	buf->buf_per_samples[idx] = nb_samples;
+
 	memcpy(buf->buf_data + buf->buf_pos, data, size);
+
+	/* move to next frame buffer */
 	buf->buf_pos += size;
+	/* mark valid length */
 	buf->buf_len = buf->buf_pos;
 
 	return 0;
@@ -61,6 +81,8 @@ int playback_buf_copy(PlaybackBuf *dst, PlaybackBuf *src, int32_t size)
 
 	if (size > src->buf_size)
 		return -EINVAL;
+
+	memcpy(dst->buf_per_samples, src->buf_per_samples, src->buf_per_count * sizeof(int32_t));
 
 	memcpy(dst->buf_data, src->buf_data, size);
 	dst->buf_len = size;
@@ -78,7 +100,7 @@ int playback_buf_try_full(PlaybackBuf *buf, int32_t write_size)
 	return (buf->buf_pos + write_size > buf->buf_size);
 }
 
-int pcm_buf_len_set(PlaybackBuf *buf, int32_t len)
+int playback_buf_len_set(PlaybackBuf *buf, int32_t len)
 {
 	if (!buf)
 		return -EINVAL;
@@ -88,7 +110,7 @@ int pcm_buf_len_set(PlaybackBuf *buf, int32_t len)
 	return 0;
 }
 
-int pcm_buf_pos_reset(PlaybackBuf *buf)
+int playback_buf_pos_reset(PlaybackBuf *buf)
 {
 	if (!buf)
 		return -EINVAL;
@@ -98,7 +120,7 @@ int pcm_buf_pos_reset(PlaybackBuf *buf)
 	return 0;
 }
 
-int pcm_buf_pos_get(PlaybackBuf *buf)
+int playback_buf_pos_get(PlaybackBuf *buf)
 {
 	return buf->buf_pos;
 }
@@ -108,9 +130,10 @@ int playback_buf_flush(PlaybackBuf *buf)
 	if (!buf)
 		return -EINVAL;
 
-	buf->buf_len = 1;
+	buf->buf_len = 0;
 	buf->buf_pos = 0;
 	memset(buf->buf_data, 0x00, buf->buf_size);
+	memset(buf->buf_per_samples, 0x00, buf->buf_per_count * sizeof(int32_t));
 
 	return 0;
 }
--- ffmpeg_sdl_audio_player/playback_buf.h
+++ ffmpeg_sdl_audio_player/playback_buf.h
@@ -2,25 +2,44 @@
 
 #include "stdafx.h"
 
+/*
+ *   frame    frame    frame
+ * ----------------------------
+ * |        |        |        |
+ * |  data  |  data  |  data  |
+ * |        |        |        |
+ * ----------------------------
+ *          ^
+ *          |
+ *         pos
+ *
+ */
 typedef struct PlaybackBuf {
 	uint8_t							*buf_data;
 	int32_t							buf_pos;
-	int32_t							buf_len;
-	int32_t							buf_size;
+	int32_t							buf_len;		/* valid size */
+	int32_t							buf_size;		/* buffer size */
+
+	int32_t							buf_per_size;
+	int32_t							buf_per_count;
+	int32_t							*buf_per_samples;	/* nb_samples of per frame */
 } PlaybackBuf;
 
 PlaybackBuf *playback_buf_alloc(void);
 void playback_buf_free(PlaybackBuf **buf);
 
-int playback_buf_init(PlaybackBuf *buf, int32_t buf_size);
+int playback_buf_init(PlaybackBuf *buf, int32_t per_size, int32_t multiple);
 void playback_buf_deinit(PlaybackBuf *buf);
 
-int playback_buf_write(PlaybackBuf *buf, uint8_t *data, int32_t size);
+int playback_buf_write(PlaybackBuf *buf, uint8_t *data, int32_t size, int32_t nb_samples);
 int playback_buf_copy(PlaybackBuf *dst, PlaybackBuf *src, int32_t size);
 
 int playback_buf_is_full(PlaybackBuf *buf);
 int playback_buf_try_full(PlaybackBuf *buf, int32_t write_size);
 
-int pcm_buf_len_set(PlaybackBuf *buf, int32_t len);
-int pcm_buf_pos_get(PlaybackBuf *buf);
+int playback_buf_len_set(PlaybackBuf *buf, int32_t len);
+
+int playback_buf_pos_reset(PlaybackBuf *buf);
+int playback_buf_pos_get(PlaybackBuf *buf);
+
 int playback_buf_flush(PlaybackBuf *buf);
